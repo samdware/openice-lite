10bd0ba1dae534db96645bd9c79aad62ef9a5e05
Hung Nguyen <hungng@seas.upenn.edu>
2018-01-17 11:15:47 -0500

    Added core source

---
+++ middleware/core/src/main/java/edu/upenn/cis/precise/openicelite/middleware/core/IMiddlewareCallback.java
@@ -0,0 +1,25 @@
+package edu.upenn.cis.precise.openicelite.middleware.core;
+
+/**
+ * Allow an application to be notified when asynchronous events
+ * (message arrives, connection lost, etc.) occur
+ *
+ * @author Hung Nguyen (hungng@seas.upenn.edu)
+ */
+public interface IMiddlewareCallback {
+    /**
+     * Handle new message from middleware as a byte array
+     *
+     * @param topic the topic the message was delivered from
+     * @param message the payload as a byte array
+     */
+    void handleMessage(String topic, byte[] message);
+
+    /**
+     * Handle new message from middleware as a string
+     *
+     * @param topic the topic the message was delivered from
+     * @param message the payload as a string
+     */
+    void handleMessage(String topic, String message);
+}
---
+++ middleware/core/src/main/java/edu/upenn/cis/precise/openicelite/middleware/core/ShutdownHook.java
@@ -0,0 +1,20 @@
+package edu.upenn.cis.precise.openicelite.middleware.core;
+
+/**
+ * Shutdown hook to handle graceful shutdown
+ *
+ * @author Hung Nguyen (hungng@seas.upenn.edu)
+ */
+public class ShutdownHook extends Thread {
+    private final IGracefulShutdown client;
+
+    public ShutdownHook(IGracefulShutdown client) {
+        this.client = client;
+    }
+
+    @Override
+    public void run() {
+        client.disconnect();
+        client.close();
+    }
+}
