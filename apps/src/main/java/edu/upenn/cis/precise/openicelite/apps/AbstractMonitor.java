24e66238b0b0ed467936613adbdc36ff05a1ffd8
Hung Nguyen <hungng@seas.upenn.edu>
2018-01-18 15:35:35 -0500

    Added AbstractMonitor

---
+++ apps/src/main/java/edu/upenn/cis/precise/openicelite/apps/AbstractMonitor.java
@@ -0,0 +1,193 @@
+package edu.upenn.cis.precise.openicelite.apps;
+
+import edu.upenn.cis.precise.openicelite.drivers.core.DeviceInfo;
+import edu.upenn.cis.precise.openicelite.drivers.core.IDriver;
+import edu.upenn.cis.precise.openicelite.drivers.core.IDriverCallback;
+import edu.upenn.cis.precise.openicelite.middleware.core.IMiddleware;
+import edu.upenn.cis.precise.openicelite.middleware.core.ShutdownHook;
+import edu.upenn.cis.precise.openicelite.middleware.mqtt.Dongle;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.UUID;
+
+/**
+ * Lightweight application to collect data from medical device and publish
+ * JSON format message to OpenICE-lite MQTT broker
+ * <p>
+ * This abstract class also includes an example of main application implementation.
+ *
+ * @author Hung Nguyen (hungng@seas.upenn.edu)
+ */
+public class AbstractMonitor {
+  private static final String DONGLE_ID = UUID.randomUUID().toString();
+  private static final String PROPERTIES_FILE_NAME = "device.properties";
+
+  private static final Logger logger = LogManager.getLogger(AbstractMonitor.class);
+
+  // Dongle configuration
+  private final String dongleId;
+  private DeviceInfo deviceInfo;
+  private HashMap<String, Object> options;
+
+  private IMiddleware middleware;
+  private IDriver driver;
+
+  public static void main(String[] args) {
+    logger.info("Starting Masimo Dongle...");
+
+    // Load configuration
+    ClassLoader loader = AbstractMonitor.class.getClassLoader();
+    URL propResource = loader.getResource(PROPERTIES_FILE_NAME);
+    File propFile = new File("./" + PROPERTIES_FILE_NAME);
+
+    String dongleId = DONGLE_ID;
+    String devicePort = null;
+    DeviceInfo deviceInfo = new DeviceInfo();
+    HashMap<String, Object> options = new HashMap<>();
+
+    if (propFile.isFile() || propResource != null) {
+      logger.info("Loading configuration from " +
+          (propFile.isFile() ? "./" : "default ") + PROPERTIES_FILE_NAME + "...");
+      try (InputStream input = (propFile.isFile() ?
+          new FileInputStream("./" + PROPERTIES_FILE_NAME) :
+          loader.getResourceAsStream(PROPERTIES_FILE_NAME))) {
+        Properties properties = new Properties();
+        properties.load(input);
+
+        // -- dongle ID
+        dongleId = properties.getProperty("dongle_id");
+        logger.info("-- Dongle ID  : " + dongleId);
+        // -- device port
+        devicePort = properties.getProperty("device_port");
+        logger.info("-- Device Port: " + devicePort);
+        // -- device ID
+        deviceInfo.setDeviceId(properties.getProperty("device_id"));
+        logger.info("-- Device ID  : " + deviceInfo.getDeviceId());
+        // -- device type
+        deviceInfo.setDeviceType(properties.getProperty("device_type"));
+        logger.info("-- Device Type: " + deviceInfo.getDeviceType());
+
+        if (dongleId == null || devicePort == null
+            || deviceInfo.getDeviceId() == null
+            || deviceInfo.getDeviceType() == null) {
+          throw new IllegalArgumentException("Invalid properties file!");
+        }
+
+        // -- other properties
+        Enumeration<?> p = properties.propertyNames();
+        while (p.hasMoreElements()) {
+          String key = (String) p.nextElement();
+          String value = properties.getProperty(key);
+          options.put(key, value);
+        }
+      } catch (Exception ex) {
+        logger.error("Failed to load configuration!", ex);
+        System.exit(-1);
+      }
+    } else {
+      logger.error("Cannot find configuration file - " + PROPERTIES_FILE_NAME + "!");
+      System.exit(-1);
+    }
+
+    // Start Dongle
+    AbstractMonitor monitor = new AbstractMonitor(dongleId, deviceInfo, options);
+
+    try {
+      monitor.startMiddleware();
+      Runtime.getRuntime().addShutdownHook(new ShutdownHook(monitor.middleware));
+    } catch (Exception ex) {
+      logger.error("Failed to start MQTT client!", ex);
+      System.exit(-1);
+    }
+
+    /* Sample code block to initialize driver
+    try {
+      monitor.startDriver(driver);
+    } catch (Exception ex) {
+      logger.error("Failed to start driver!", ex);
+      System.exit(-1);
+    }
+    */
+  }
+
+  /**
+   * Monitor constructor
+   *
+   * @param dongleId   dongle unique ID (UUID)
+   * @param deviceInfo information of connected medical devices
+   * @param options    additional options to initialize MQTT dongle
+   */
+  public AbstractMonitor(String dongleId, DeviceInfo deviceInfo, HashMap<String, Object> options) {
+    this.dongleId = dongleId;
+    this.deviceInfo = deviceInfo;
+    this.options = options;
+  }
+
+  /**
+   * Initialize middleware object, then connect to MQTT broker
+   */
+  public void startMiddleware() {
+    middleware = new Dongle(dongleId);
+    middleware.init(options);
+    middleware.connect(null, null, null);
+  }
+
+  /**
+   * Initialize driver to connect to the medical device, then subscribe to data stream
+   *
+   * @param driver IDriver instance to communicate with medical device
+   */
+  public void startDriver(IDriver driver) {
+    this.driver = driver;
+    DriverCallback callback = new DriverCallback(deviceInfo.getDeviceId(), middleware);
+    driver.subscribe(null, callback);
+    middleware.addDevice(deviceInfo);
+  }
+
+  private class DriverCallback implements IDriverCallback {
+    private final String deviceId;
+    private final IMiddleware middleware;
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
+
+    DriverCallback(String deviceId, IMiddleware middleware) {
+      this.deviceId = deviceId;
+      this.middleware = middleware;
+    }
+
+    /**
+     * This demo doesn't need to handle byte array input
+     *
+     * @param message data to be handled
+     */
+    @Override
+    public void handleMessage(byte[] message) {
+    }
+
+    /**
+     * Handle new data from driver as a string
+     *
+     * @param message data to be handled
+     */
+    @Override
+    public void handleMessage(String message) {
+      long now = System.currentTimeMillis();
+      JSONObject json = new JSONObject();
+      json.put("time", dateFormat.format(now));
+      json.put("epoch", new Long(now));
+      json.put("data", message);
+
+      middleware.publishId(deviceId, json.toString(), null);
+    }
+  }
+}
